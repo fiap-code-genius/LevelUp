9c85bccf3bef9c79bde96a9f78c407cc2daffbec
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 18:44:12 -0300

    feat: Criando as classes comuns da aplicação para controle de operações e de paginação

---
+++ LevelUp.Domain/Common/PageResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LevelUp.Domain.Common
+{
+    public class PageResultModel<T>
+    {
+        public required T Data { get; set; }
+        public int Offset { get; set; }
+        public int Take { get; set; }
+        public int Total { get; set; }
+    }
+}
