d31b7aab13e4cd0fd78585adb3f4db1394425548
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 18:47:04 -0300

    feat: Criando classes de erro para mapeamento personalizado

---
+++ LevelUp.Domain/Errors/IdNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace LevelUp.Domain.Errors
+{
+    public class IdNotFoundException : Exception
+    {
+        public IdNotFoundException() { }
+
+        public IdNotFoundException(string? message) : base(message)
+        {
+            Console.WriteLine($"Erro - {message} - {DateTime.UtcNow.ToString()}");
+        }
+    }
+}
---
+++ LevelUp.Domain/Errors/NoContentException.cs
@@ -0,0 +1,14 @@
+﻿namespace LevelUp.Domain.Errors
+{
+    public class NoContentException : Exception
+    {
+        public NoContentException()
+        {
+        }
+
+        public NoContentException(string? message) : base(message)
+        {
+            Console.WriteLine($"Erro - {message} - {DateTime.UtcNow.ToString()}");
+        }
+    }
+}
