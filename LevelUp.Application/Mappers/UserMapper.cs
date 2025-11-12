e2ac66023fb7dd16ef53e1d8bfc289f820502de5
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 21:47:42 -0300

    feat: Criando um mapper para traduzir os dtos em entidades e vice-versa

---
+++ LevelUp.Application/Mappers/UserMapper.cs
@@ -0,0 +1,35 @@
+﻿using LevelUp.Application.Dtos.User;
+using LevelUp.Domain.Entities;
+
+namespace LevelUp.Application.Mappers
+{
+    public static class UserMapper
+    {
+
+        public static UserEntity ToEntity(this UserCreateDto dto, string passwordHash)
+        {
+            return new UserEntity
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                PasswordHash = passwordHash,
+                JobTitle = dto.JobTitle,
+                TeamId = dto.TeamId,
+                Role = "USER"
+            };
+        }
+
+        public static UserResponseDto ToResponseDto(this UserEntity user)
+        {
+            return new UserResponseDto(
+                            user.Id,
+                            user.FullName,
+                            user.Email,
+                            user.JobTitle,
+                            user.PointBalance,
+                            user.Role,
+                            user.TeamId
+                        );
+        }
+    }
+}
