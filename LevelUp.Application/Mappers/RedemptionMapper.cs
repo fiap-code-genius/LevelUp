258d04185dcc46b93e666820f29376b5e13d2b4f
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 21:54:23 -0300

    feat: Criando a mapper para Redemption

---
+++ LevelUp.Application/Mappers/RedemptionMapper.cs
@@ -0,0 +1,20 @@
+﻿using LevelUp.Application.Dtos.Redemption;
+using LevelUp.Domain.Entities;
+
+namespace LevelUp.Application.Mappers
+{
+    public static class RedemptionMapper
+    {
+
+        public static RedemptionResponseDto ToResponseDto(this RewardRedemptionEntity redemption)
+        {
+            return new RedemptionResponseDto(
+                redemption.Id,
+                redemption.RewardId,
+                redemption.Reward.Name,
+                redemption.PointsSpent,
+                redemption.RedeemedAt
+            );
+        }
+    }
+}
