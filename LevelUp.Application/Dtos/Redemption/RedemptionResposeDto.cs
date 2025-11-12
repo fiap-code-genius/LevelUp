4a343cec29ff76dda154ffb7bdf4f0b86e24c869
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 21:40:37 -0300

    feat: Criando o DTO de resposta para Redemption

---
+++ LevelUp.Application/Dtos/Redemption/RedemptionResposeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LevelUp.Application.Dtos.Redemption
+{
+    public record RedemptionResponseDto(
+            int Id,
+            int RewardId,
+            string RewardName,
+            int PointsSpent,
+            DateTime RedeemedAt
+        );
+}
