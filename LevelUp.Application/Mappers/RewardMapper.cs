338b0f85f6a88814be784a281cf37f7e1225fc35
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 21:49:53 -0300

    feat: Criando um mapper para Reward

---
+++ LevelUp.Application/Mappers/RewardMapper.cs
@@ -0,0 +1,31 @@
+﻿using LevelUp.Application.Dtos.Reward;
+using LevelUp.Domain.Entities;
+
+namespace LevelUp.Application.Mappers
+{
+    public static class RewardMapper
+    {
+
+        public static RewardEntity ToEntity(this RewardCreateUpdateDto dto)
+        {
+            return new RewardEntity
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                PointCost = dto.PointCost,
+                StockQuantity = dto.StockQuantity
+            };
+        }
+
+        public static RewardResponseDto ToResponseDto(this RewardEntity reward)
+        {
+            return new RewardResponseDto(
+                            reward.Id,
+                            reward.Name,
+                            reward.Description,
+                            reward.PointCost,
+                            reward.StockQuantity
+                        );
+        }
+    }
+}
