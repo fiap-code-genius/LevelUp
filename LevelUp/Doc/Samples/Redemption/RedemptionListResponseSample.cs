870fe707fe9578988e2670da9e5130fd7ab10d41
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-14 17:39:51 -0300

    doc: Criando exemplos de resposta para RewardRedemption

---
+++ LevelUp/Doc/Samples/Redemption/RedemptionListResponseSample.cs
@@ -0,0 +1,38 @@
+﻿using LevelUp.Application.Dtos.Redemption;
+using LevelUp.Domain.Common;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace LevelUp.Doc.Samples.Redemption
+{
+    public class RedemptionListResponseSample : IExamplesProvider<PageResultModel<IEnumerable<RedemptionResponseDto>>>
+    {
+        public PageResultModel<IEnumerable<RedemptionResponseDto>> GetExamples()
+        {
+            var redemptionList = new List<RedemptionResponseDto>
+            {
+                new RedemptionResponseDto(
+                    Id: 1,
+                    RewardId: 5,
+                    RewardName: "Gift Card iFood R$100",
+                    PointsSpent: 600,
+                    RedeemedAt: DateTime.UtcNow.AddDays(-2)
+                ),
+                new RedemptionResponseDto(
+                    Id: 2,
+                    RewardId: 7,
+                    RewardName: "Créditos na Alura",
+                    PointsSpent: 100,
+                    RedeemedAt: DateTime.UtcNow.AddDays(-15)
+                )
+            };
+
+            return new PageResultModel<IEnumerable<RedemptionResponseDto>>
+            {
+                Data = redemptionList,
+                Offset = 0,
+                Take = 10,
+                Total = 2
+            };
+        }
+    }
+}
---
+++ LevelUp/Doc/Samples/Redemption/RedemptionResponseSample.cs
@@ -0,0 +1,19 @@
+﻿using LevelUp.Application.Dtos.Redemption;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace LevelUp.Doc.Samples.Redemption
+{
+    public class RedemptionResponseSample : IExamplesProvider<RedemptionResponseDto>
+    {
+        public RedemptionResponseDto GetExamples()
+        {
+            return new RedemptionResponseDto(
+                Id: 1,
+                RewardId: 5,
+                RewardName: "Gift Card iFood R$100",
+                PointsSpent: 600,
+                RedeemedAt: DateTime.UtcNow.AddMinutes(-5)
+            );
+        }
+    }
+}
